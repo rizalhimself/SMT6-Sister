ca009814cce3182693e1469773a8e3c982d9198b
rizalhimself <rizalhimself@gmail.com>
2022-03-14 17:36:45 +0700

    P2 Aplikasi Perpustakaan - Membuat method pada formBuku.cs

---
+++ Praktikum2/P2_Sister_11116/formBuku.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace P2_Sister_11116
+{
+    public partial class formBuku : Form
+    {
+        public formBuku()
+        {
+            InitializeComponent();
+        }
+
+        private void btExit_Click(object sender, EventArgs e)
+        {
+            formHome frmHome = new formHome();
+            frmHome.Show();
+            this.Close();
+        }
+    }
+}
---
+++ Praktikum2/P2_Sister_11116/formHome.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace P2_Sister_11116
+{
+    public partial class formHome : Form
+    {
+        public formHome()
+        {
+            InitializeComponent();
+        }
+
+        private void dataBukuToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            formBuku frmBk = new formBuku();
+            frmBk.Show();
+            this.Hide();
+        }
+
+        private void dataPeminjamToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            formPeminjam frmPj = new formPeminjam();
+            frmPj.Show();
+            this.Hide();
+        }
+
+        private void closeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Environment.Exit(1);
+        }
+    }
+}
